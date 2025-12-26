9376f48bb6d05276d96a4bf7391b208a239e287c
thedaripanator <sayondeep1618@gmail.com>
2025-12-26 09:54:46 +0530

    Added update and delete option in admin panel

--- demo/src/main/java/com/quiz/demo/controller/AdminController.java
+++ demo/src/main/java/com/quiz/demo/controller/AdminController.java
@@ -2,10 +2,7 @@ package com.quiz.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.quiz.demo.model.Question;
 import com.quiz.demo.service.QuizService;
@@ -23,13 +20,28 @@ public class AdminController {
     @GetMapping
     public String adminPage(Model model) {
         model.addAttribute("question", new Question());
+        model.addAttribute("questions", quizService.getAllQuestions());
         model.addAttribute("questionCount", quizService.getQuestionCount());
         return "admin";
     }
 
-    @PostMapping("/add")
-    public String addQuestion(@ModelAttribute("question") Question question) {
+    @PostMapping("/save")
+    public String saveQuestion(@ModelAttribute("question") Question question) {
         quizService.saveQuestion(question);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editQuestion(@PathVariable int id, Model model) {
+        model.addAttribute("question", quizService.getQuestionById(id));
+        model.addAttribute("questions", quizService.getAllQuestions());
+        model.addAttribute("questionCount", quizService.getQuestionCount());
+        return "admin";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteQuestion(@PathVariable int id) {
+        quizService.deleteQuestion(id);
         return "redirect:/admin";
     }
 }
